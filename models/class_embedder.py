30a6f6fe59ced7eebbff64a6c581bc9981d9926b
opertw <taoruiw@andrew.cmu.edu>
2025-04-15 14:08:43 -0700

    Implement CFG

--- models/class_embedder.py
+++ models/class_embedder.py
@@ -7,7 +7,7 @@ class ClassEmbedder(nn.Module):
         super().__init__()
         
         # TODO: implement the class embeddering layer for CFG using nn.Embedding
-        self.embedding = None 
+        self.embedding = nn.Embedding(n_classes, embed_dim)
         self.cond_drop_rate = cond_drop_rate
         self.num_classes = n_classes
 
@@ -16,8 +16,8 @@ class ClassEmbedder(nn.Module):
         
         if self.cond_drop_rate > 0 and self.training:
             # TODO: implement class drop with unconditional class
-            x = None
+            x = torch.randint(0, self.num_classes, (b,), device=x.device)
         
         # TODO: get embedding: N, embed_dim
-        c = None 
+        c = self.embedding(x)
         return c
